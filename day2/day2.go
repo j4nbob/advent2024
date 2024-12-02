41c83e15909b87ae2efb3f929e162763f7abdfea
Jan Bob <j4nbob@gmail.com>
2024-12-01 20:39:21 -0800

    prep day2 files

---
+++ day2/day2.go
@@ -0,0 +1,11 @@
+// day2 of Advent Of Code 2024 : https://adventofcode.com/2024/day/1
+// j4nbob
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Day 2 of Advent Of Code 2024, j4nbob")
+}
